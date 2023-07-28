07c1a4a770214c8fe9131c2428eab0dfcce44699
quangbilly79 <quang.billy.79@gmail.com>
2023-07-28 17:57:06 +0700

    28/07/2023

--- src/main/java/javacourse/section3OOP/Main.java
+++ src/main/java/javacourse/section3OOP/Main.java
@@ -3,14 +3,24 @@ package javacourse.section3OOP;
 public class Main {
     public static void main (String[] args) {
         Motorbike honda = new Motorbike("honda", "gear1", 0);
-        System.out.println(honda.gear); // gear1
-        System.out.println(honda.speed); // 0
-        honda.adjustGear("gear2");
-        honda.adjustSpeed(3);
+        System.out.println(honda.getGear()); // gear1
+        System.out.println(honda.getSpeed()); // 0
+        honda.setGear("gear2");
+        honda.setSpeed(3);
         System.out.println(honda.gear); // gear2
         System.out.println(honda.speed); // 3
         honda.speed=10;
-        System.out.println(honda.speed); // 10
+        System.out.println(honda.getSpeed()); // 10
+        honda.increaseSpeed(5);
+        System.out.println(honda.getSpeed()); // 15
+
+        Motorbike honda1 = new Motorbike("honda1");
+        System.out.println(honda1.getGear()); // defaultGear
+
+        // test default value trong TH k init giá trị
+        Motorbike blank = new Motorbike();
+        System.out.println(blank.speed); // 0
+        System.out.println(blank.gear); // null
 
     }
 }
--- src/main/java/javacourse/section3OOP/Motorbike.java
+++ src/main/java/javacourse/section3OOP/Motorbike.java
@@ -4,21 +4,47 @@ public class Motorbike {
     // Cac field
     public String name;
     public String gear;
-    public Integer speed;
+    public int speed;
 
     // Constructor method
-    public Motorbike(String name, String gear, Integer speed) {
+    public Motorbike(String name, String gear, int speed) {
         this.name = name;
         this.gear = gear;
         this.speed = speed;
     }
+    // Constructor thứ 2 overload bên trên để set default value
+    public Motorbike(String name) {
+        this.name = name;
+        this.gear = "defaultGear";
+        this.speed = 3;
+        // Hoặc this(name, "defaultGear", 3)
+    }
+
+    // Blank Constructor để test default value
+    public Motorbike() {
+    }
+
     // Chinh gear
-    public void adjustGear(String newGear) {
+    public void setGear(String newGear) {
         this.gear = newGear;
     }
-    // Chinh Speed
-    public void adjustSpeed(Integer newSpeed) {
-        this.speed = newSpeed;
+    // Chinh Speed, check dk
+    public void setSpeed(int newSpeed) {
+        if (newSpeed > 0) {
+            this.speed = newSpeed;
+        }
+    }
+
+    // Get Speed va` Gear
+    public int getSpeed() {
+        return this.speed;
+    }
+    public String getGear() {
+        return this.gear;
+    }
+    // Tang Speed. Sử dụng method setSpeed để check đk luôn
+    public void increaseSpeed(int speed) {
+        setSpeed(this.speed + speed);
     }
 }
 
