2fc0d4581bc08c2fb66875389992108c6d20167e
quangbilly79 <quang.billy.79@gmail.com>
2023-09-19 17:33:22 +0700

    19/09/2023 mobi

--- src/main/java/javacourse/section20OOPpart2/Interface.java
+++ src/main/java/javacourse/section20OOPpart2/Interface.java
@@ -1,10 +1,14 @@
 package javacourse.section20OOPpart2;
 
 public interface Interface {
+    // Field trong Interface mặc định là final
     int i = 0;
     String s = "a";
     // Tương tự abstract, method k đc có body.
     // Khác ở chỗ Luôn luôn là public
+    // K cần từ khóa abstract
+    // Ngoài ra k đc chứa method thg giống như abstract class
     void method1();
     void method2(int a);
+
 }
