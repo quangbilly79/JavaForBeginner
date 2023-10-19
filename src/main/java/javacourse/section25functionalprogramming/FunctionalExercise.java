84f1771039f3c9fb889a2e718084fdacd1b9c810
quangbilly79 <quang.billy.79@gmail.com>
2023-10-19 17:44:24 +0700

    19/10/2023 vega

---
+++ src/main/java/javacourse/section25functionalprogramming/FunctionalExercise.java
@@ -0,0 +1,70 @@
+package javacourse.section25functionalprogramming;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+class forEachInteger implements Consumer<Integer> {
+    @Override
+    public void accept(Integer integer) {
+        System.out.println(integer*integer);
+    }
+}
+
+class mapInteger implements Function<Integer, String> {
+    @Override
+    public String apply(Integer integer) {
+        return integer.toString() + "_abc";
+    }
+}
+// Method reference: class::func
+class methodReferrenceTest {
+    public static void printCustom(Integer i) {
+        System.out.println(i);
+    }
+    public static boolean isEven(Integer i) {
+        return i%2 == 0;
+    }
+    public static Integer maxOf(Integer i1, Integer i2) {
+        return Integer.max(i1, i2);
+    }
+    public static int comapreTo(Integer i1, Integer i2) {
+        if (i1 > i2) {
+            return -1;
+        } else if (i1 == i2) {
+            return 0;
+        }
+        return 1;
+    }
+}
+
+public class FunctionalExercise {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        Collections.addAll(integerList,1,3,4,2,5,5,4);
+        integerList.stream().forEach(new forEachInteger());
+        System.out.println("-----------");
+        integerList.stream().map(new mapInteger()).forEach(System.out::println);
+        //1_abc
+        //3_abc
+        //4_abc ...
+        // Vd đơn giản về Method reference
+        integerList.stream().map(i -> i.byteValue());
+        integerList.stream().map(Integer::byteValue);
+        // Vd về custom Method reference
+        integerList.stream().forEach(i -> methodReferrenceTest.printCustom(i));
+        integerList.stream().forEach(methodReferrenceTest::printCustom);
+
+        integerList.stream().filter(methodReferrenceTest::isEven);
+
+        integerList.stream().max(methodReferrenceTest::maxOf);
+        System.out.println("-----------");
+        integerList.stream().sorted(methodReferrenceTest::comapreTo).forEach(System.out::println);
+
+
+
+
+    }
+}
--- src/main/java/javacourse/section25functionalprogramming/FunctionalTest.java
+++ src/main/java/javacourse/section25functionalprogramming/FunctionalTest.java
@@ -4,6 +4,10 @@ package javacourse.section25functionalprogramming;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FunctionalTest {
     // Viết thành 1 hàm tính tổng cho 1 list
@@ -16,9 +20,15 @@ public class FunctionalTest {
                 }
                 );
     }
+
+    // Return function trong 1 function
+    public static Predicate<Integer> funcFuncPredicate() {
+        return x -> x % 2 == 0;
+    }
+
     public static void main(String[] args) {
         List<Integer> integerList = new ArrayList<>();
-        Collections.addAll(integerList,1,3,4,2,5);
+        Collections.addAll(integerList,1,3,4,2,5,5,4);
         // convert thành stream để dùng các hàm như forEach, filter, map,... giống Scala
         // Lambda function elem -> funcx(elem). 1 vd về pass 1 hàm làm tham số
         integerList.stream().forEach(elem -> System.out.printf(elem + " "));
@@ -41,9 +51,58 @@ public class FunctionalTest {
         integerList.stream().reduce(Integer.MIN_VALUE, Integer::max);
         integerList.stream().reduce(Integer.MIN_VALUE, (elem1, elem2) -> Integer.max(elem1, elem2));
 
-        integerList.stream().min(Integer::compareTo);
+        integerList.stream().min(Integer::compareTo).orElse(2);
+
+        // original list: 1,3,4,2,5,5,4
+        // sorted: trả về stream, có thể truyền vào Comparator
+        integerList.stream().sorted().forEach(elem -> System.out.printf(elem + " "));
+        // 1 2 3 4 4 5 5
+        System.out.printf("\n-----\n");
+        integerList.stream().sorted(Integer::compareTo).forEach(elem -> System.out.printf(elem + " "));
+        System.out.printf("\n-----\n");
+
+        // distinct: trả về stream chỉ chứa ptử distinct
+        integerList.stream().distinct().forEach(elem -> System.out.printf(elem + " "));
+        // 1 3 4 2 5
+        System.out.printf("\n-----\n");
+
+        // map: trả về stream sau khi áp func vs từng ptu
+        integerList.stream().map(elem -> elem * elem).forEach(elem -> System.out.printf(elem + " "));
+        // 1 9 16 4 25 25 16
+        System.out.printf("\n-----\n");
+        // Có thể kết hợp kiểu .sorted().distinct().map().foreach()
+
+        // Convert sang List với .collect(Collectors.toList()/toSet()/toCollection())
+        List<Integer> integerList1 = integerList.stream().collect(Collectors.toList());
+
+        System.out.println(integerList1); // [1, 3, 4, 2, 5, 5, 4]
+        // Convert sang Array với .toArray(Integer[]::new) (k có param sẽ trả về Object[])
+        // Param đc gọi là generator, public interface IntFunction<R>
+        Integer[] integerList2 = integerList.stream().toArray(Integer[]::new);
+        System.out.println(integerList2); // [1, 3, 4, 2, 5, 5, 4]
 
 
+        // in mũ 2 của 10 số từ 1 đến 10
+        // Dùng IntSream.range(i1,i2) để tạo 1 stream bđ vs i1 va kết thúc vs i2-1
+        IntStream.range(1,10).forEach(elem -> System.out.printf(elem*elem + " "));
+        System.out.printf("\n-----\n");
+        // in lower case của tất cả ký tự
+        List<String> stringList = new ArrayList<>();
+        Collections.addAll(stringList, "aB", "BB", "ac", "Ca");
+        stringList.stream().forEach(elem -> System.out.printf(elem.toLowerCase() + " "));
+        System.out.printf("\n-----\n");
+        // in do. dai` cac' tu`
+        stringList.stream().forEach(elem -> System.out.printf(elem.length() + " "));
+
+        // Gán function cho 1 biến
+        Predicate<Integer> funcValPredicate = x -> x % 2 == 0;
+        Consumer<String> funcValConsumer = System.out::printf;
+        System.out.printf("\n-----\n");
+        integerList.stream().filter(funcValPredicate).forEach(System.out::println);
+        System.out.printf("\n-----\n");
+
+
+        integerList.stream().filter(funcFuncPredicate()).forEach(System.out::println);
 
 
 
