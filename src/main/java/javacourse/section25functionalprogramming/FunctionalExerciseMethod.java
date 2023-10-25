befc220c575ead71c9c74ed973ae5ed3beb5e1d1
quangbilly79 <quang.billy.79@gmail.com>
2023-10-25 17:13:58 +0700

    25/10/2023 vega

---
+++ src/main/java/javacourse/section25functionalprogramming/FunctionalExerciseMethod.java
@@ -0,0 +1,25 @@
+package javacourse.section25functionalprogramming;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FunctionalExerciseMethod {
+    /**
+     * This method filters out the odd numbers from a given list of integers.
+     * @param numbers This is the list of integers from which odd numbers are to be filtered out.
+     * @return List<Integer> This returns a list of odd integers.
+     */
+    public static List<Integer> filterOddNumbers(List<Integer> numbers) {
+        return numbers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
+    }
+    /**
+     * This method generates the cubes of the first n natural numbers.
+     * @param n The number of natural numbers for which cubes are to be calculated.
+     * @return List<Integer> This returns a list of cubes of the first n natural numbers.
+     */
+    public static List<Integer> getCubesOfFirstNNumbers(int n) {
+        return IntStream.range(1, n+1).boxed().map(i -> i*i*i).collect(Collectors.toList());
+    }
+}
